bf795ef8b1d4e7618014bff1b9bfd745efdd9358
CJOYBLUV <dave_hemmer@hotmail.com>
2015-08-11 09:59:22 -0700

    refactor for movies controller

--- controllers/favorites.js
+++ controllers/favorites.js
@@ -5,19 +5,12 @@ var router = express.Router();
 
 router.post('/', function(req, res) {
 
-  // db.favorite.create({ imdbid: req.body.imdbId, title: req.body.title, year: req.body.year, poster: req.body.poster })
-  //          .then(function(data) {
-  //             console.log('<<<  FAVORITE CREATED >>>');
-  //             res.redirect('/movies/'+data.imdbid);
-  //   });
-
   db.favorite
     .findOrCreate({where: { imdbid: req.body.imdbId, title: req.body.title, year: req.body.year, poster: req.body.poster }})
     .spread(function(favorite, created) {
       console.log(favorite); // returns info
       res.redirect('/movies/'+favorite.imdbid);
     });
-
   // res.send(req.body);
 });
 
@@ -56,57 +49,5 @@ router.post('/:id/comments', function(req,res) {
 });
 
 
-// // POSTS /posts - create new post
-// router.post('/',function(req, res){
-//   db.author.findById(req.body.author).then(function(author){
-//     author.createPost({
-//       title:req.body.title,
-//       content:req.body.content
-//     }).then(function(post){
-//       res.redirect('/posts');
-//     });
-//   });
-//   // res.send(req.body);
-// });
-
-
-
-// db.favorite.find(
-//   {where:{imdbid:'tt0903624'} })
-// .then(function(favorite){
-//   favorite.getComments().then(function(comments){
-//        console.log('comment count',comments.length);
-//        comments.forEach(function(comment){
-//       console.log('------');
-//       console.log(comment.get());
-
-//   });
-//   console.log(favorite);
-//   });
-// });
-
-
-  // db.author.findAll().then(function(authors){
-  // db.post.findAll({include:[db.author]}).then(function(posts){
-  //     res.render('posts/index',{
-  //       myPosts:posts,
-  //       authorList:authors
-  //     });
-  //   });
-  // });
-
-
-// // POSTS /posts - create new post
-// router.post('/',function(req, res){
-//   db.favorite.findById(req.body.favorite).then(function(favorite){
-//     favorite.createPost({
-//       title:req.body.title,
-//       content:req.body.content
-//     }).then(function(post){
-//       res.redirect('/posts');
-//     });
-//   });
-//   // res.send(req.body);
-// });
 
 module.exports = router;
---
+++ controllers/movies.js
@@ -0,0 +1,74 @@
+var db = require('../models');
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+// my movie constructor
+function Movie(imdbID, Title, Year, Type) {
+  this.imdbID = imdbID;
+  this.Title = Title;
+  this.Year = Year;
+  this.Type = Type;
+}
+// {
+// Title: "The Star Wars Holiday Special",
+// Year: "1978",
+// imdbID: "tt0193524",
+// Type: "movie"
+// }
+
+// movie index
+router.get("/", function(req, res) {
+  // res.send(req.query.q);
+  var q = req.query.q
+  // var movies = [];
+
+  if (q) {
+    // res.send(q);
+    // http://www.omdbapi.com/?s=matrix
+    var url = 'http://www.omdbapi.com/?s='+q;
+    request(url, function(error, response, data) {
+      var parsedData = JSON.parse(data);
+      // res.send(parsedData.Search);
+      if (parsedData.Search) {
+
+// /movies?q=star+wars
+         res.render('movies/index', {
+            movies: parsedData.Search,
+            q: q
+         });
+      } else {
+        console.log('no results!');
+        res.redirect('/');
+      }
+    });
+  } else {
+    res.redirect('/');
+  }
+});
+
+// show movie details page
+router.get("/:imdbID", function(req, res) {
+   // res.send (req.params);
+
+    var imdbID = req.params.imdbID;
+    // http://www.omdbapi.com/?i=tt0133093
+    var url = 'http://www.omdbapi.com/?plot=full&tomatoes=true&r=json&i='+imdbID;
+    request(url, function(error, response, data) {
+      var parsedData = JSON.parse(data);
+      // res.send(parsedData);
+
+      if (parsedData) {
+        res.render('movies/show', {
+            movie: parsedData,
+            prevPage: req.headers['referer']
+         });
+      } else {
+        console.log('no results!');
+        res.redirect('/movies/index');
+      }
+    });
+});
+
+
+module.exports = router;
